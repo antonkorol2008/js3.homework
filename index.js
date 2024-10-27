5b6940c9280d1d579199da034d6a29455b6ea54d
antonkorol2008 <antonkorol6r@gmail.com>
2024-10-27 12:59:26 +0200

    add.js3

---
+++ index.js
@@ -0,0 +1,30 @@
+let result =5+5+'5'
+console.log(result);
+let email = 'antonkorol6r@gmail.com'; 
+
+
+if (email.includes('@')) 
+    console.log('@');
+
+
+
+let length = email.length;
+console.log(email.length);
+ 
+let Viktor1 = 'My';
+let Viktor2 = 'name';
+let Viktor3 = 'is';
+
+
+let fullName = Viktor1 + ' ' + Viktor2 + ' ' + Viktor3 + ' Viktor';
+
+console.log(fullName); 
+let userName = 'Олександре'; 
+let payment = 300; 
+
+
+alert(`Дякуємо, До сплати 300 гривень.`);
+
+
+
+    
